aa9591055d7b930e87447e521d3b5363157d56f0
Umang A <umanga.eng@gmail.com>
2025-04-18 12:23:20 -0400

    backend connected

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        // BACKEND_API: `${process.env.NEXT_PUBLIC_APP_BACKEND}`,
-        BACKEND_API: 'http://localhost:5000'
+        BACKEND_API: `${process.env.NEXT_PUBLIC_APP_BACKEND}`,
+        // BACKEND_API: 'http://localhost:5000'
       }
 };
 
